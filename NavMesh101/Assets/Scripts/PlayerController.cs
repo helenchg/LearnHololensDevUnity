bc6507eeda987ad2d713da83d0f21e38c2fe602c
helenchg <helenchg93@hotmail.com>
2019-12-04 21:35:43 -0500

    Learning about navmesh

---
+++ NavMesh101/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerController : MonoBehaviour
+{
+    private Camera cam;
+    public NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    
+    void Start()
+    {
+        cam = Camera.main;
+        agent.updateRotation = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                // Move Agent
+                agent.SetDestination(hit.point);
+            }
+        }
+        if(agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+
+        } else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
